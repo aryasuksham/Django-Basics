50b6a24fbf00f0204bca603feaba3fb76762b513
aryasuksham <sukshamarya.itwaves@gmail.com>
2023-11-14 19:43:31 +0530

    update, delete, built-in authentication

--- core/settings.py
+++ core/settings.py
@@ -125,6 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR, "public/static")
+}
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "public/static")
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,9 @@ from django.contrib import admin
 from django.urls import path
 from home.views import *
 from vege.views import *
+from django.conf.urls.static import static
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
@@ -27,6 +30,18 @@ urlpatterns = [
     path('about/', about, name = 'about'),
     path('contact/', contact, name = 'contact'),
 
-    path('receipes/', receipes, name = 'receipes')
+    path('recipes/', recipes, name = 'recipes'),
+    path('delete_recipe/<id>/', delete_recipe, name = 'delete_recipe'),
+    path('update_recipe/<id>/', update_recipe, name = 'update_recipe'),
+
+    path('login/', login_page, name = 'login_page'),
+    path('logout/', logout_page, name = 'logout_page'), 
+    path('register/', register_page, name = 'register_page'),
 
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
